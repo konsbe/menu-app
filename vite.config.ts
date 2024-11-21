48be7c8d35da2321fb62a0771e9a1e5d6a983a5c
k.berdelis <k.berdelis@deepsea.ai>
2024-11-21 20:45:10 +0200

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  base: "/menu-app/"
-})
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      manifest: {
+        name: "My PWA App",
+        short_name: "PWA App",
+        description: "My Progressive Web App built with Vite",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/menu-app/",
+});
